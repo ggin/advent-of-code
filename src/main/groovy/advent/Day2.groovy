2a5df339ae3874e4985a104493f0748b2be8fc72
Guillaume Giner <guillaume.giner@starlingbank.com>
2020-12-02 11:39:12 +0000

    Refactoring the parsing

--- src/main/groovy/advent/Day2.groovy
+++ src/main/groovy/advent/Day2.groovy
@@ -7,23 +7,25 @@ class Day2 {
     def static VALUES = Day1.class.getResource("/day2-input.txt").readLines()
 
     static long puzzle1() {
-        VALUES.collect { (it =~ PATTERN).with { parseLine(it) } }
+        VALUES.collect {  parseLine(it) }
                 .count { it.with { (x..y).contains(t.count(l)) } }
     }
 
     static long puzzle2() {
-        VALUES.collect { (it =~ PATTERN).with { parseLine(it) } }
+        VALUES.collect {parseLine(it) }
         .count{it.with {t[x - 1] == l ^ t[y - 1] == l } }
     }
 
-    private static Expando parseLine(Matcher it) {
-        it.matches()
-        new Expando(
-                x: it.group('x') as int,
-                y: it.group('y') as int,
-                l: it.group('l'),
-                t: it.group('t')
-        )
+    static Expando parseLine(String line) {
+        (line =~ PATTERN).with {
+            it.matches()
+            new Expando(
+                    x: it.group('x') as int,
+                    y: it.group('y') as int,
+                    l: it.group('l'),
+                    t: it.group('t')
+            )
+        }
     }
 
     static void main(String[] args) {
