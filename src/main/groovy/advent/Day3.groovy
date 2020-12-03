d6933953e59a4f28c52d84e87abad9145e636449
Guillaume Giner <guillaume.giner@starlingbank.com>
2020-12-03 08:55:50 +0000

    Day 3 - initial solution

--- src/main/groovy/advent/Day3.groovy
+++ src/main/groovy/advent/Day3.groovy
@@ -2,12 +2,35 @@ package advent
 
 class Day3 implements DailyChallenge {
 
+    /*
+    Right 1, down 1.
+Right 3, down 1. (This is the slope you already checked.)
+Right 5, down 1.
+Right 7, down 1.
+Right 1, down 2.
+     */
     long puzzle1(List<String> values) {
-        0
+        def m = values.collect { it.split('').collect { it == "." ? 0 : 1 } }
+        nbTrees(m, 3, 1)
+    }
+
+    long nbTrees(def m, int right, int down) {
+        def x = 0
+        def y = 0
+        def trees = 0
+        while (y < m.size()) {
+            if (m[y][x] == 1) {
+                trees++
+            }
+            x = (x + right) % m[y].size()
+            y += down
+        }
+        trees
     }
 
     long puzzle2(List<String> values) {
-        0
+        def m = values.collect { it.split('').collect { it == "." ? 0 : 1 } }
+        nbTrees(m, 1, 1) * nbTrees(m, 3, 1) * nbTrees(m, 5, 1) * nbTrees(m, 7, 1) * nbTrees(m, 1, 2)
     }
 
 }
--- src/test/groovy/advent/Day3Test.groovy
+++ src/test/groovy/advent/Day3Test.groovy
@@ -2,9 +2,19 @@ package advent
 
 class Day3Test extends DailyChallengeTest {
     {
-        input = []
-        expectedResultPuzzle1 = 0
-        expectedResultPuzzle2 = 0
+        input = ["..##.......",
+                 "#...#...#..",
+                 ".#....#..#.",
+                 "..#.#...#.#",
+                 ".#...##..#.",
+                 "..#.##.....",
+                 ".#.#.#....#",
+                 ".#........#",
+                 "#.##...#...",
+                 "#...##....#",
+                 ".#..#...#.#"]
+        expectedResultPuzzle1 = 7
+        expectedResultPuzzle2 = 336
         classToTest = Day3
     }
 }
