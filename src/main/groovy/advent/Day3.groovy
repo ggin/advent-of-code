6746ac11514d043d2825f771e8d4fba68ccaeb34
Guillaume Giner <guillaume.giner@starlingbank.com>
2020-12-03 09:50:01 +0000

    Simplifying solution

--- src/main/groovy/advent/Day3.groovy
+++ src/main/groovy/advent/Day3.groovy
@@ -2,35 +2,16 @@ package advent
 
 class Day3 implements DailyChallenge {
 
-    /*
-    Right 1, down 1.
-Right 3, down 1. (This is the slope you already checked.)
-Right 5, down 1.
-Right 7, down 1.
-Right 1, down 2.
-     */
     long puzzle1(List<String> values) {
-        def m = values.collect { it.split('').collect { it == "." ? 0 : 1 } }
-        nbTrees(m, 3, 1)
-    }
-
-    long nbTrees(def m, int right, int down) {
-        def x = 0
-        def y = 0
-        def trees = 0
-        while (y < m.size()) {
-            if (m[y][x] == 1) {
-                trees++
-            }
-            x = (x + right) % m[y].size()
-            y += down
-        }
-        trees
+        nbTrees(values, 3, 1)
     }
 
     long puzzle2(List<String> values) {
-        def m = values.collect { it.split('').collect { it == "." ? 0 : 1 } }
-        nbTrees(m, 1, 1) * nbTrees(m, 3, 1) * nbTrees(m, 5, 1) * nbTrees(m, 7, 1) * nbTrees(m, 1, 2)
+        [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]].collect { r, d -> nbTrees(values, r, d)}.inject { x, y -> x * y}
+    }
+
+    long nbTrees(List<String> v, int right, int down) {
+        (0..<v.size() / down).count { v[it*down][it*right % v[0].size()] == '#'}
     }
 
 }
