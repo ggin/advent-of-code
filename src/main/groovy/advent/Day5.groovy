91de04081ef7d59ccdadf5e135423a93b0d9666a
Guillaume Giner <guillaume.giner@starlingbank.com>
2020-12-04 14:42:50 +0000

    Preparing day 5 solution

---
+++ src/main/groovy/advent/Day5.groovy
@@ -0,0 +1,11 @@
+package advent
+
+class Day5 implements DailyChallenge {
+
+    long puzzle1(String text) {
+    }
+
+    long puzzle2(String text) {
+    }
+
+}
---
+++ src/test/groovy/advent/Day5Test.groovy
@@ -0,0 +1,10 @@
+package advent
+
+class Day5Test extends DailyChallengeTest {
+    {
+        input = ""
+        expectedResultPuzzle1 = 0
+        expectedResultPuzzle2 = 0
+        classToTest = Day5
+    }
+}
