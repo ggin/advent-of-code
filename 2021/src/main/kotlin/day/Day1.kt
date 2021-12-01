7355adf8cd4f65d705cb5f33db8fde94a24d1dc2
Guillaume Giner <guillaume.giner@starlingbank.com>
2021-12-01 09:28:26 +0000

    Improving Day 1 solution

--- 2021/src/main/kotlin/day/Day1.kt
+++ 2021/src/main/kotlin/day/Day1.kt
@@ -4,17 +4,13 @@ import common.DailyChallenge
 
 class Day1 : DailyChallenge {
 
-    override fun puzzle1(values: List<String>): Long {
-        val input = toLongValues(values)
-        return input.filterIndexed { index, x ->
-            index != input.size - 1 && x < input[index + 1]
-        }.count().toLong()
-    }
+    override fun puzzle1(values: List<String>) = countLargerMeasurements(toLongValues(values), 1)
 
-    override fun puzzle2(values: List<String>): Long {
-        val input = toLongValues(values)
-        return input.filterIndexed { index, x ->
-            index < input.size - 1  && index > 1 && input[index - 2] < input[index + 1]
-        }.count().toLong()
+    override fun puzzle2(values: List<String>) = countLargerMeasurements(toLongValues(values), 3)
+
+    private fun countLargerMeasurements(measurements: List<Long>, indexOffset: Int): Long {
+        return measurements.dropLast(indexOffset)
+            .mapIndexed { index, _ -> Pair(index, index + indexOffset) }
+            .count { (i, j) -> measurements[i] < measurements[j] }.toLong()
     }
 }
