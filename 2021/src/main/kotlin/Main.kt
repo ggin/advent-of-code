fdc7db6484d93306b5a3f8a7b603064573466008
Guillaume Giner <guillaume.giner@starlingbank.com>
2021-12-10 08:59:14 +0000

    Day 10 solution

--- 2021/src/main/kotlin/Main.kt
+++ 2021/src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 import day.*
 
 fun main() {
-    Day9().run()
+    Day10().run()
 }
--- 2021/src/main/kotlin/day/Day10.kt
+++ 2021/src/main/kotlin/day/Day10.kt
@@ -2,13 +2,38 @@ package day
 
 import common.DailyChallenge
 
+typealias Stack = ArrayDeque<Char>
+
 class Day10 : DailyChallenge {
 
-    override fun puzzle1(values: List<String>): Long {
-        return 0L
+    companion object {
+        val SYNTAX_CHECKER_MAPPINGS = mapOf(')' to 3L, ']' to 57L, '}' to 1197L, '>' to 25137L)
+        val AUTO_COMPLETE_MAPPINGS = mapOf(')' to 1L, ']' to 2L, '}' to 3L, '>' to 4L)
+        val CHAR_MAPPINGS = mapOf('(' to ')', '[' to ']', '{' to '}', '<' to '>')
     }
 
-    override fun puzzle2(values: List<String>): Long {
-        return 0L
-    }
+    override fun puzzle1(values: List<String>) =
+        values.mapNotNull { illegalCharacter(it.toCharArray()) }
+            .sumOf { SYNTAX_CHECKER_MAPPINGS[it]!! }
+
+    override fun puzzle2(values: List<String>) = values.map { it.toCharArray() }
+        .filter { illegalCharacter(it) == null }
+        .map { missingSequence(it) }
+        .map { calculateScore(it) }
+        .sorted().let { scores -> scores[scores.size / 2] }
+
+    private fun illegalCharacter(line: CharArray) =
+        Stack().let { line.dropWhile { c -> it.updateStack(c) }.firstOrNull() }
+
+    private fun missingSequence(line: CharArray) =
+        Stack().apply { line.forEach { updateStack(it) } }
+
+    private fun Char.isOpeningChar() = CHAR_MAPPINGS.containsKey(this)
+
+    private fun Stack.updateStack(c: Char): Boolean =
+        if (c.isOpeningChar()) add(CHAR_MAPPINGS[c]!!) else removeLast() == c
+
+    private fun calculateScore(chars: List<Char>) =
+        chars.foldRight(0L) { c, score -> score * 5 + AUTO_COMPLETE_MAPPINGS[c]!! }
+
 }
