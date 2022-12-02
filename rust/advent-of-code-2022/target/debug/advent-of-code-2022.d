736311fbf2ab520efb3e16b6358c1a4bc3dbc17f
Guillaume Giner <guillaume.giner@starlingbank.com>
2022-12-02 12:30:00 +0000

    Removing unused files

--- rust/advent-of-code-2022/target/debug/advent-of-code-2022.d
+++
@@ -1,1 +0,0 @@
-/Users/guillaume.giner/code/advent-of-code/rust/advent-of-code-2022/target/debug/advent-of-code-2022: /Users/guillaume.giner/code/advent-of-code/rust/advent-of-code-2022/src/main.rs
--- rust/advent-of-code-2022/target/debug/deps/advent_of_code_2022-4dc9759deb11c975.d
+++
@@ -1,5 +0,0 @@
-/Users/guillaume.giner/code/advent-of-code/rust/advent-of-code-2022/target/debug/deps/advent_of_code_2022-4dc9759deb11c975: src/main.rs
-
-/Users/guillaume.giner/code/advent-of-code/rust/advent-of-code-2022/target/debug/deps/advent_of_code_2022-4dc9759deb11c975.d: src/main.rs
-
-src/main.rs:
