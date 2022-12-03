0a6599ea8be7b3a092d9fa39d4d1eda1dc576c23
Guillaume Giner <guillaume.giner@starlingbank.com>
2022-12-03 08:52:16 +0000

    Day 3

--- 2022/src/main/kotlin/day/Day3.kt
+++ 2022/src/main/kotlin/day/Day3.kt
@@ -5,8 +5,16 @@ import common.Input
 
 class Day3 : DailyChallenge {
 
-    override fun puzzle1(input: Input) = 0L
+    override fun puzzle1(input: Input) = input.values
+        .map { it.chunked(it.length / 2) }
+        .map { commonChars(it[0], it[1]).first() }
+        .sumOf { toPriority(it) }
 
-    override fun puzzle2(input: Input) = 0L
+    override fun puzzle2(input: Input) = input.values
+        .chunked(3)
+        .map { commonChars(commonChars(it[0], it[1]).toString(), it[2]).first() }
+        .sumOf { toPriority(it) }
 
+    private fun commonChars(s1: String, s2: String) = s1.toSet().intersect(s2.toSet())
+    private fun toPriority(c: Char) = (c - '`').mod(58L)
 }
---
+++ 2022/src/main/kotlin/day/Day4.kt
@@ -0,0 +1,12 @@
+package day
+
+import common.DailyChallenge
+import common.Input
+
+class Day4 : DailyChallenge {
+
+    override fun puzzle1(input: Input) = 0L
+
+    override fun puzzle2(input: Input) = 0L
+
+}
