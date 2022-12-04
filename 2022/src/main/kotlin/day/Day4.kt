274d09907fcafc24011414e8e3453547e5433861
Guillaume Giner <guillaume.giner@starlingbank.com>
2022-12-04 10:52:46 +0000

    Day 4 solution

--- 2022/src/main/kotlin/day/Day4.kt
+++ 2022/src/main/kotlin/day/Day4.kt
@@ -5,8 +5,15 @@ import common.Input
 
 class Day4 : DailyChallenge {
 
-    override fun puzzle1(input: Input) = 0L
+    override fun puzzle1(input: Input) = input
+        .toPairs(",") { s -> LongRange(s.split("-")[0].toLong(), s.split("-")[1].toLong()) }
+        .count { p -> p.first.intersect(p.second).size == minOf(p.first.count(), p.second.count())  }
+        .toLong()
 
-    override fun puzzle2(input: Input) = 0L
+
+    override fun puzzle2(input: Input) = input
+        .toPairs(",") { s -> LongRange(s.split("-")[0].toLong(), s.split("-")[1].toLong()) }
+        .count { p -> p.first.intersect(p.second).isNotEmpty() }
+        .toLong()
 
 }
--- 2022/src/test/kotlin/day/Day5Test.kt
+++ 2022/src/test/kotlin/day/Day5Test.kt
@@ -4,19 +4,19 @@ import DailyChallengeTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class Day4Test : DailyChallengeTest {
+internal class Day5Test : DailyChallengeTest {
 
-    private val challenge = Day4()
+    private val challenge = Day5()
 
     @Test
     fun `puzzle 1`() {
         val result = puzzle1(challenge)
-        assertEquals(2L, result)
+        assertEquals(0L, result)
     }
 
     @Test
     fun `puzzle 2`() {
         val result = puzzle2(challenge)
-        assertEquals(4L, result)
+        assertEquals(0L, result)
     }
 }
