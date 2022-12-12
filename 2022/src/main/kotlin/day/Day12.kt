b4e8ef252acef2719ef180ab2027b699c2c4c78a
Guillaume Giner <guillaume.giner@starlingbank.com>
2022-12-12 15:57:51 +0000

    Fix

--- 2022/src/main/kotlin/day/Day12.kt
+++ 2022/src/main/kotlin/day/Day12.kt
@@ -6,7 +6,6 @@ import common.Input
 
 class Day12 : DailyChallenge {
 
-    // somehow the unit tests pass but I'm off by 2 for both parts - values returned = 352, 351 - expected = 350, 349
     override fun puzzle1(input: Input): Long {
         val distances = calculateDistances(input)
         return distances.filterKeys { it.c == 'S' }.values.first() - 1L
@@ -20,6 +19,15 @@ class Day12 : DailyChallenge {
 
     data class Position(val x: Int, val y: Int, val c: Char)
 
+
+    private fun calculateDistances(input: Input) : Map<Position, Int> {
+        val mat = input.values.map { it.toCharArray() }
+        val x = mat.indexOfFirst { it.contains('E') }
+        val y = mat[x].indexOf('E')
+        val distances = mutableMapOf<Position, Int>()
+        move(mat, null, x, y, distances)
+        return distances
+    }
 
     private fun move(
         mat: List<CharArray>,
@@ -44,20 +52,11 @@ class Day12 : DailyChallenge {
                             || (mat[x][y] == 'a' && mat[it.first][it.second] == 'S')
                             || (mat[x][y] == 'b' && mat[it.first][it.second] == 'S')
                             || mat[currentPosition.x][currentPosition.y] == mat[it.first][it.second] + 1
-                            || (mat[x][y] != 'E' && mat[it.first][it.second] != 'S' && mat[currentPosition.x][currentPosition.y] <= mat[it.first][it.second])
+                            || mat[currentPosition.x][currentPosition.y] <= mat[it.first][it.second]
                 }
                 .forEach { nextPosition ->
                     move(mat, currentPosition, nextPosition.first, nextPosition.second, distances)
                 }
         }
-    }
-
-    private fun calculateDistances(input: Input) : Map<Position, Int> {
-        val mat = input.values.map { it.toCharArray() }
-        val x = mat.indexOfFirst { it.contains('E') }
-        val y = mat[x].indexOf('E')
-        val distances = mutableMapOf<Position, Int>()
-        move(mat, null, x, y, distances)
-        return distances
     }
 }
