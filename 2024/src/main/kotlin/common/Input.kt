c0c467fa36f8459ad265b11271d0323243c48d95
Guillaume Giner <guillaume.giner@starlingbank.com>
2024-12-02 08:22:33 +0000

    Day 2 updates

--- 2024/src/main/kotlin/common/Input.kt
+++ 2024/src/main/kotlin/common/Input.kt
@@ -8,6 +8,7 @@ class Input(filepath: String) {
 
     fun toInt() = values.map { it.toInt() }
     fun toLong() = values.map { it.toLong() }
+    fun toLongList(sep: String = " ") = values.map { it.split(sep).map { s -> s.toLong() } }.toList()
     fun toStringPairs() = values.map { it.split(" ") }.map { Pair(it[0], it[1]) }
     fun toCharPairs() = values.map { it.toCharArray() }.map { Pair(it[0], it[2]) }
     fun <T> toPairs(sep: String = " ", transform: (line: String) -> T) =
--- 2024/src/main/kotlin/day/Day2.kt
+++ 2024/src/main/kotlin/day/Day2.kt
@@ -4,38 +4,27 @@ import common.DailyChallenge
 import common.Input
 
 class Day2 : DailyChallenge {
-    override fun puzzle1(input: Input): Long {
-        return input.values
-            .map { it.split(" ").map { it.toLong() }.toList() }
-            .count { isSafe(it) || isSafe(it.reversed()) }
-            .toLong()
-    }
+    override fun puzzle1(input: Input) = input.toLongList()
+        .count { isSafe(it) || isSafe(it.reversed()) }
+        .toLong()
 
-    private fun isSafe(l: List<Long>): Boolean {
-        if (l.size == 1)
-            return true
-        if (l[1] - l[0] in 1..3)
-            return isSafe(l.drop(1))
-        return false
-    }
+    override fun puzzle2(input: Input) = input.toLongList()
+        .count {
+            isSafe(it, 1)
+                    || isSafe(it.drop(1), 0)
+                    || isSafe(it.reversed(), 1)
+                    || isSafe(it.reversed().drop(1), 0)
+        }
+        .toLong()
 
-    override fun puzzle2(input: Input): Long {
-        return input.values
-            .map { it.split(" ").map { it.toLong() }.toList() }
-            .count { isSafeWithTolerance(it, 1, true) || isSafeWithTolerance(it.reversed(), 1, true) }
-            .toLong()
-    }
-
-    private fun isSafeWithTolerance(l: List<Long>, tolLevel: Int, isFirstRecursion: Boolean = false): Boolean {
+    private fun isSafe(l: List<Long>, tolLevel: Int = 0): Boolean {
         if (tolLevel == -1)
             return false
         if (l.size == 1)
             return true
         return if (l[1] - l[0] in 1..3)
-            isSafeWithTolerance(l.drop(1), tolLevel)
-        else if (isFirstRecursion)
-            isSafeWithTolerance(listOf(l.first()) + l.drop(2), tolLevel - 1) || isSafeWithTolerance(l.drop(1), tolLevel - 1)
+            isSafe(l.drop(1), tolLevel)
         else
-            isSafeWithTolerance(listOf(l.first()) + l.drop(2), tolLevel - 1)
+            isSafe(listOf(l.first()) + l.drop(2), tolLevel - 1)
     }
 }
